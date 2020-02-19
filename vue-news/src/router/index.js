b2e6740ca84434013bcce9232da13771758898e5
seungpang <obey1342@gmail.com>
2020-02-19 23:48:24 +0900

    redirect, router-link

--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
-import Jobsview from '../views/Jobsview.vue';
+import JobsView from '../views/JobsView.vue';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
     routes: [
+        {
+            path: '/',
+            redirect: '/news'
+        },
         {// path: url주소
             path: '/news',
             //component: url 주소로 갔을 때 표시될 컴포넌트
@@ -19,7 +23,7 @@ export const router = new VueRouter({
         },
         {
             path: '/jobs',
-            component: Jobsview,
+            component: JobsView,
         }
 
     ]
